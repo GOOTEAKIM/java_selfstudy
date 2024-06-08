b217ddb461fd905fa6d2fb0a55a448fe8b57fed1
김구태 <gootae0220@naver.com>
2024-06-09 01:21:19 +0900

    chapter 1,2 end

---
+++ java-self-study/src/CommentJava.java
@@ -0,0 +1,11 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java111"); // hello java1 출력
+
+        /*
+        System.out.println("hello java2");
+        System.out.println("hello java3");
+        */
+
+    }
+}
